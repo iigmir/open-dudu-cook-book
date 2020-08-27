460553c13b3422caaed08f870068df225fa5cbdf
iigmir <roc120j@gmail.com>
2020-08-27 18:28:17 +0800

    Develop features

--- src/renderer/views/Menu/feature.vue
+++ src/renderer/views/Menu/feature.vue
@@ -3,28 +3,47 @@
         <div class="theme list">
             <h1 class="title is-4">主題列表</h1>
             <ul>
-                <li>A</li>
-                <li>A</li>
-                <li>A</li>
+                <li v-for="(theme, theme_id) in themes" v-bind:key="theme_id">
+                    <a v-on:click="idx = theme_id" href="javascript:;">{{ theme.$.name }}</a>
+                </li>
             </ul>
         </div>
-        <div class="sub-dish list">
-            <h1 class="subtitle is-5">家常小菜</h1>
+        <div v-if="idx >= 0" class="sub-dish list">
+            <h1 class="subtitle is-5">{{ current_theme.$.name }}</h1>
+            <ul>
+                <li v-for="(cti_item, cti_id) in current_theme_items" v-bind:key="cti_id">
+                    <a v-on:click="click_event('Feature', cti_item)" href="javascript:;">{{ cti_item }}</a>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
 import emits from "./child-emits.js";
-import {  mapState } from "vuex";
+import { mapState } from "vuex";
 
 export default {
     name: "Feature",
     mixins: [ emits ],
+    data: () => ({
+        idx: -1,
+    }),
     computed: {
-        ... mapState({
-            themes: state => state.Recipes.default_theme
-        })
+        ...mapState({
+            default_theme: state => state.Recipes.default_theme
+        }),
+        themes() {
+            return this.default_theme.alltheme.theme;
+        },
+        current_theme() {
+            return this.idx < 0 ? {} : this.themes[this.idx];
+        },
+        current_theme_items() {
+            return this.current_theme.item ?
+                this.current_theme.item.split("|").filter(t=>t.length>0) :
+                [];
+        }
     }
 };
 </script>
