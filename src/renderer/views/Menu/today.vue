31a4f793d3923a86294be2036559adb9069c1612
iigmir <roc120j@gmail.com>
2020-08-27 18:47:41 +0800

    today change

--- src/renderer/views/Menu/today.vue
+++ src/renderer/views/Menu/today.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="today">
-        <div class="rec" v-on:click="click_event('Today','recipe')">今日食譜</div>
-        <div class="fod" v-on:click="click_event('Today','food')">今日菜籃</div>
+        <div class="rec" v-on:click="click_event('Today','recipe')">
+            <h1 class="title is-4">今日食譜</h1>
+        </div>
+        <div class="fod" v-on:click="click_event('Today','food')">
+            <h1 class="title is-4">今日菜籃</h1>
+        </div>
     </div>
 </template>
 
